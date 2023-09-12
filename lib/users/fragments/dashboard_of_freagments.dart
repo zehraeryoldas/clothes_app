50509c68fb98eb510ed272eae61d0b914f9f44de
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-12 14:26:43 +0300

    DashBoardOFFragmetns page created

---
+++ lib/users/fragments/dashboard_of_freagments.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DashBoardOfFragments extends StatelessWidget {
+  const DashBoardOfFragments({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+      ),
+    );
+  }
+}
