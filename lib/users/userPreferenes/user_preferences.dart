4db6508fb7811e25309cb1f673e48adddbf71eb1
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-13 01:24:41 +0300

    rememberuserpreference comment lines added

--- lib/users/userPreferenes/user_preferences.dart
+++ lib/users/userPreferenes/user_preferences.dart
@@ -6,9 +6,30 @@ import 'package:shared_preferences/shared_preferences.dart';
 class RememberUserPreferences {
   static Future<void> saveRememberUser(User userInfo) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    String userJsonData =
-        jsonEncode(userInfo.toJson()); //verileri json formatında iletiyoruz.
-    await sharedPreferences.setString("currentUser",
-        userJsonData); //currentUser adıyla paylaşılan tercihleri kullanarak telefonun yerel depolama alanına sunuyoruz
+
+    // Kullanıcı bilgilerini JSON formatına dönüştürüyoruz
+    String userJsonData = jsonEncode(userInfo.toJson());
+
+    // JSON verisini "currentUser" adıyla SharedPreferences tercihlerine kaydediyoruz
+    await sharedPreferences.setString("currentUser", userJsonData);
+  }
+
+  static Future<User?> readUserInfo() async {
+    User? currentUserInfo;
+
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    // "currentUser" adıyla kaydedilmiş kullanıcı bilgileri metnini alıyoruz
+    String? userInfo = sharedPreferences.getString("currentUser");
+
+    // Eğer kayıtlı kullanıcı bilgileri varsa
+    // JSON metnini bir  (Map) olarak çözümlüyoruz
+    Map<String, dynamic> userDataMap = jsonDecode(userInfo!);
+
+    // Çözümlenen JSON verisini User.fromJson ile User sınıfına dönüştürüyoruz
+    currentUserInfo = User.fromJson(userDataMap);
+
+    // Okunan kullanıcı bilgilerini döndürüyoruz (Eğer kayıtlı bilgi yoksa null döner)
+    return currentUserInfo;
   }
 }
