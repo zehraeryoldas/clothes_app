b6c3771d5d111772db82c6fb791d36d3da2f4012
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-13 01:24:21 +0300

    currentuser class created

---
+++ lib/users/userPreferenes/current_user.dart
@@ -0,0 +1,29 @@
+import 'package:clothes_app/users/model/user.dart';
+import 'package:clothes_app/users/userPreferenes/user_preferences.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class CurrentUser extends GetxController {
+  // Mevcut kullanıcının verilerini yönetmek için GetxController sınıfını kullanıyoruz.
+
+  final Rx<User> _currentUser = User(0, '', '', '').obs;
+  // Rx<User> türünde bir değişken oluşturuyoruz. Bu değişken, kullanıcı verilerini içerecek.
+  // .obs ile bu değişkenin değişikliklerini dinleyebilir hale getiriyoruz.
+
+  User get user => _currentUser.value;
+  // _currentUser değişkeninin değerine erişim sağlayan bir get metodu tanımlıyoruz.
+  // Bu, mevcut kullanıcı verilerine erişmek için kullanılır.
+
+  getUserInfo() async {
+    // getUserInfo adında bir asenkron metot tanımlıyoruz.
+    // Bu metot, kullanıcı bilgilerini almak için kullanılacak.
+
+    User? getUserInfoFromLocalStorage =
+        await RememberUserPreferences.readUserInfo();
+    // RememberUserPreferences sınıfından kullanıcı bilgilerini alıyoruz. Bu işlem asenkron olduğu için await kullanıyoruz.
+    // getUserInfoFromLocalStorage, kullanıcı bilgilerini içerir veya null olabilir.
+
+    _currentUser.value = getUserInfoFromLocalStorage!;
+    // _currentUser değişkenini, yerel depodan okunan kullanıcı bilgileriyle güncelliyoruz.
+  }
+}
