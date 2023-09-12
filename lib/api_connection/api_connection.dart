311abacefa331055f7fd63e860acb03626aef2db
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-12 13:50:10 +0300

    login.php import

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -5,4 +5,5 @@ class API {
   //signUp user
   static const validateEmail = "$hostConnect/user/validate_email.php";
   static const signUp = "$hostConnect/user/signup.php";
+  static const login = '$hostConnect/user/login.php';
 }
