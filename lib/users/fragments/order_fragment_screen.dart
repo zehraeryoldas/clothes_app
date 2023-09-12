128ad2466f7659478da5c593beab5a28979a4ed9
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-13 01:24:08 +0300

    fragment pagescreated

---
+++ lib/users/fragments/order_fragment_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OrderFragmentScreen extends StatelessWidget {
+  const OrderFragmentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Order Fragment Screen"),
+      ),
+    );
+  }
+}
