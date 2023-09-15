c47ff8d69792fb8fa61318e2892e18edf90ddb44
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-15 22:12:11 +0300

    api url changed

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -1,9 +1,13 @@
 class API {
-  static const hostConnect = "http://192.168.1.100/api_clothes_store";
+  static const hostConnect = "http://192.168.1.9/api_clothes_store";
   static const hostConnectUser = "$hostConnect/user";
+  static const hostConnectAdmin = "$hostConnect/admin";
 
   //signUp user
   static const validateEmail = "$hostConnect/user/validate_email.php";
   static const signUp = "$hostConnect/user/signup.php";
   static const login = '$hostConnect/user/login.php';
+
+  //login admin
+  static const loginAdmin = "$hostConnect/admin/login.php";
 }
