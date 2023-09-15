0ab7f2c5098490533f0384d25ff4c172c241ef5b
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-15 22:12:03 +0300

    imagepicker function added

--- lib/admin/admin_upload_items.dart
+++ lib/admin/admin_upload_items.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AdminUploadItemsScreen extends StatefulWidget {
   const AdminUploadItemsScreen({super.key});
@@ -8,12 +12,156 @@ class AdminUploadItemsScreen extends StatefulWidget {
 }
 
 class _AdminUploadItemsScreenState extends State<AdminUploadItemsScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("blabla"),
+  final ImagePicker _picker = ImagePicker();
+  XFile? pickedImageXFile;
+
+  captureImageWithPhoneCamera() async {
+    pickedImageXFile = await _picker.pickImage(source: ImageSource.camera);
+    Get.back();
+    setState(() {
+      pickedImageXFile;
+    });
+  }
+
+  pickImageFromPhoneGallery() async {
+    pickedImageXFile = await _picker.pickImage(source: ImageSource.gallery);
+    Get.back();
+    setState(() {
+      pickedImageXFile;
+    });
+  }
+
+  showDialogBoxForImagePickingAndCapturing() {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            backgroundColor: Colors.black54,
+            title: const Text(
+              "Item image",
+              style: TextStyle(
+                  color: Colors.deepPurple, fontWeight: FontWeight.bold),
+            ),
+            children: [
+              SimpleDialogOption(
+                onPressed: () {
+                  captureImageWithPhoneCamera();
+                },
+                child: const Text(
+                  "Capture with Phone Camera",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  pickImageFromPhoneGallery();
+                },
+                child: const Text(
+                  "Pick image From Phone Gallery",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Get.back();
+                },
+                child: const Text(
+                  "Cancel",
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
+              )
+            ],
+          );
+        });
+  }
+
+  Widget defaultScreen() {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Colors.black54,
+            Colors.deepPurple,
+          ])),
+        ),
+        automaticallyImplyLeading: false,
+        title: const Text("Welcome Admin"),
+        centerTitle: true,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Colors.black54,
+          Colors.deepPurple,
+        ])),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.add_a_photo,
+                color: Colors.white54,
+                size: 200,
+              ),
+              Material(
+                color: Colors.black38,
+                borderRadius: BorderRadius.circular(30),
+                child: InkWell(
+                  onTap: () {
+                    showDialogBoxForImagePickingAndCapturing();
+                  },
+                  borderRadius: BorderRadius.circular(30),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      "Add New Item",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  Widget uploadItemFormScreen() {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: ListView(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.3,
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: FileImage(
+                      File(pickedImageXFile!.path),
+                    ),
+                    fit: BoxFit.cover)),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return pickedImageXFile == null ? defaultScreen() : uploadItemFormScreen();
   }
 }
