420c58e6c83b982b96ad0a496f3c3e3c7356b261
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-15 13:48:33 +0300

    adminUploadItemScreen created

---
+++ lib/admin/admin_upload_items.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AdminUploadItemsScreen extends StatefulWidget {
+  const AdminUploadItemsScreen({super.key});
+
+  @override
+  State<AdminUploadItemsScreen> createState() => _AdminUploadItemsScreenState();
+}
+
+class _AdminUploadItemsScreenState extends State<AdminUploadItemsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("blabla"),
+      ),
+    );
+  }
+}
