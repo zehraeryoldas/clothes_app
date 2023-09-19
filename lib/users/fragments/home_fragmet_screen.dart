666f40d0e3c37c44c7712ef845208b9ba07422ab
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-19 17:31:26 +0300

    HomeFragmentScreen rebuild

--- lib/users/fragments/home_fragmet_screen.dart
+++ lib/users/fragments/home_fragmet_screen.dart
@@ -1,13 +1,94 @@
 import 'package:flutter/material.dart';
 
 class HomeFragmentScreen extends StatelessWidget {
-  const HomeFragmentScreen({super.key});
+  HomeFragmentScreen({super.key});
+
+  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Fragment Screen"),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 16,
+          ),
+          //search bar widget
+          showSearchBarWidget(),
+          const SizedBox(
+            height: 26,
+          ),
+          //trending-popular items
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 18),
+            child: Text(
+              "Trending",
+              style: TextStyle(
+                  color: Colors.purpleAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24),
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          //all new collections/items
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 18),
+            child: Text(
+              "New Collections",
+              style: TextStyle(
+                  color: Colors.purpleAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget showSearchBarWidget() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 18,
+      ),
+      child: TextField(
+        style: const TextStyle(color: Colors.white),
+        controller: searchController,
+        decoration: InputDecoration(
+            prefixIcon: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.purpleAccent,
+              ),
+            ),
+            hintText: "Search best clothes here..",
+            hintStyle: const TextStyle(
+              color: Colors.grey,
+              fontSize: 12,
+            ),
+            suffixIcon: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.shopping_cart,
+                  color: Colors.purpleAccent,
+                )),
+            border: const OutlineInputBorder(
+              borderSide: BorderSide(width: 2, color: Colors.purpleAccent),
+            ),
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(width: 2, color: Colors.purpleAccent),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(width: 2, color: Colors.green),
+            ),
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            fillColor: Colors.grey,
+            filled: true),
       ),
     );
   }
