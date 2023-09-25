47ac3de88c918a93a8ebaa5e4828fe2bb4d1bd3a
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-26 01:06:37 +0300

    ItemDetailsController helper created

---
+++ lib/users/controller/item_details_controller.dart
@@ -0,0 +1,29 @@
+import 'package:get/get.dart';
+
+class ItemDetailsController extends GetxController {
+  final RxInt _quantityItem = 1.obs;
+  final RxInt _sizeItem = 0.obs;
+  final RxInt _colorItem = 0.obs;
+  final RxBool _isFavorite = false.obs;
+
+  int get quantity => _quantityItem.value;
+  int get size => _sizeItem.value;
+  int get color => _colorItem.value;
+  bool get favorite => _isFavorite.value;
+
+  setQuantityItem(int quantity) {
+    _quantityItem.value = quantity;
+  }
+
+  setSizeItem(int size) {
+    _sizeItem.value = size;
+  }
+
+  setColorItem(int color) {
+    _colorItem.value = color;
+  }
+
+  setIsFavorite(bool isFavorite) {
+    _isFavorite.value = isFavorite;
+  }
+}
