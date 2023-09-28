c193a88b00fcb189a0a40990a45a0220a066f63c
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-28 17:06:43 +0300

    fields added and getxcontroller provided

---
+++ lib/users/controller/cart_list_controller.dart
@@ -0,0 +1,46 @@
+import 'package:clothes_app/users/model/cart.dart';
+import 'package:get/get.dart';
+
+class CartListController extends GetxController {
+  final RxList<Cart> _cartList = <Cart>[].obs;
+  final RxList<int> _selectedItem = <int>[].obs;
+  final RxBool _isSelectedAll = false.obs;
+  final RxDouble _total = 0.0.obs;
+
+  List<Cart> get cartList => _cartList.value;
+  List<int> get selectedItem => _selectedItem.value;
+  bool get isSelectedAll => _isSelectedAll.value;
+  double get total => _total.value;
+
+  setList(List<Cart> list) {
+    _cartList.value = list;
+  }
+
+//sepete ürün ekleme kodu
+  addSelectedItem(int itemSelectedID) {
+    _selectedItem.value.add(itemSelectedID);
+    update();
+  }
+
+//sepetten ürün silme kodu
+  deleteSelectedITem(int itemSelectedID) {
+    _selectedItem.value.remove(itemSelectedID);
+    update();
+  }
+
+//sepetteki tüm ürünleri seçme kodu
+  setIsSelectedAll() {
+    _isSelectedAll.value = !_isSelectedAll.value;
+  }
+
+//sepeti temizleme
+  clearAllSelectedItems() {
+    _selectedItem.value.clear();
+    update();
+  }
+
+//toplam miktar hesaplama
+  setTotal(double overallTotal) {
+    _total.value = overallTotal;
+  }
+}
