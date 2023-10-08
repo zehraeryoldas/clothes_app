f87669f46abc0339f657540eca9545c55865deb7
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-10-08 08:30:30 +0300

    içerik düzenlemesi yapıldı

--- lib/users/fragments/favorites_fragment_screen.dart
+++ lib/users/fragments/favorites_fragment_screen.dart
@@ -5,9 +5,35 @@ class FavoriteFragmentScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Favorite Fragment Screen"),
+    return const SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(16, 24, 8, 8),
+            child: Text(
+              "My Favorite List: ",
+              style: TextStyle(
+                  color: Colors.purpleAccent,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(16, 24, 8, 8),
+            child: Text(
+              "Order there best clothes for yourself now. ",
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          //displaying favorite list
+        ],
       ),
     );
   }
