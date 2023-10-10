bc2e7ad8a6157a73e49324e5982a8a75a4d71543
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-10-10 18:31:30 +0300

    page created

--- lib/users/fragments/favorites_fragment_screen.dart
+++ lib/users/fragments/favorites_fragment_screen.dart
@@ -1,7 +1,46 @@
+import 'dart:convert';
+
+import 'package:clothes_app/api_connection/api_connection.dart';
+import 'package:clothes_app/users/model/favorite.dart';
+import 'package:clothes_app/users/userPreferenes/current_user.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:http/http.dart' as http;
 
 class FavoriteFragmentScreen extends StatelessWidget {
-  const FavoriteFragmentScreen({super.key});
+  FavoriteFragmentScreen({super.key});
+  final currentOnlineUser = Get.put(CurrentUser());
+
+  Future<List<Favorite>> getCurrentFavoriteList() async {
+    //en son favorileri getirmeyi sağlayacaktık
+    List<Favorite> favoriteListOfCurrentUser = [];
+
+    try {
+      var res = await http.post(Uri.parse(API.readToFavorite), body: {
+        'user_id': currentOnlineUser.user.user_id.toString(),
+      });
+      if (res.statusCode == 200) {
+        var resBodyOfGetCurrentUserFavoriteListItems = jsonDecode(res.body);
+        if (resBodyOfGetCurrentUserFavoriteListItems["success"] == true) {
+          for (var eachCurrentUserCartItemData
+              in resBodyOfGetCurrentUserFavoriteListItems[
+                  'currentUserFavoriteData']) {
+            favoriteListOfCurrentUser
+                .add(Favorite.fromJson(eachCurrentUserCartItemData));
+          }
+        } else {
+          Fluttertoast.showToast(msg: "Error occured");
+        }
+      } else {
+        Fluttertoast.showToast(msg: "Status is not 200");
+      }
+    } catch (e) {
+      print("Error:: $e");
+    }
+    return favoriteListOfCurrentUser;
+  }
 
   @override
   Widget build(BuildContext context) {
