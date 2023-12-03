ae11e935f9618cf24f3cb1336a9bdd9f8c3f64a1
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-12-03 20:59:52 +0300

    contents chagnged

--- lib/users/fragments/order_fragment_screen.dart
+++ lib/users/fragments/order_fragment_screen.dart
@@ -1,184 +1,281 @@
 import 'dart:convert';
 
-import 'package:clothes_app/api_connection/api_connection.dart';
 import 'package:clothes_app/users/model/order.dart';
+import 'package:clothes_app/users/order/order_details.dart';
 import 'package:clothes_app/users/userPreferenes/current_user.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 
+import '../../api_connection/api_connection.dart';
+
 class OrderFragmentScreen extends StatelessWidget {
-  OrderFragmentScreen({super.key});
   final currentOnlineUser = Get.put(CurrentUser());
-  Future<List<Order>> getCurrentOrderList() async {
-    //en son favorileri getirmeyi sağlayacaktık
-    List<Order> orderListOfCurrentUser = [];
+
+  OrderFragmentScreen({super.key});
+
+  Future<List<Order>> getCurrentUserOrdersList() async {
+    List<Order> ordersListOfCurrentUser = [];
 
     try {
       var res = await http.post(Uri.parse(API.readOrder), body: {
         "currentOnlineUserID": currentOnlineUser.user.user_id.toString(),
       });
+
       if (res.statusCode == 200) {
-        var resBodyOfGetCurrentUserOrderListItems = jsonDecode(res.body);
-        if (resBodyOfGetCurrentUserOrderListItems["success"] == true) {
-          //eğer başarılı dönerse alamamız gerekn kayoıtlar var
-          for (var eachCurrentUserOrderItemData
-              in (resBodyOfGetCurrentUserOrderListItems["currentUserOrdersData"]
+        var responseBodyOfCurrentUserOrdersList = jsonDecode(res.body);
+
+        if (responseBodyOfCurrentUserOrdersList['success'] == true) {
+          for (var eachCurrentUserOrderData
+              in (responseBodyOfCurrentUserOrdersList['currentUserOrdersData']
                   as List)) {
-            orderListOfCurrentUser
-                .add(Order.fromJson(eachCurrentUserOrderItemData));
+            ordersListOfCurrentUser
+                .add(Order.fromJson(eachCurrentUserOrderData));
           }
-        } else {
-          Fluttertoast.showToast(msg: "Error occured");
         }
       } else {
-        Fluttertoast.showToast(msg: "Status is not 200");
+        Fluttertoast.showToast(msg: "Status Code is not 200");
       }
-    } catch (e) {
-      print("Error:: $e");
+    } catch (errorMsg) {
+      Fluttertoast.showToast(msg: "Error:: $errorMsg");
     }
-    //mevcut kullanıcı favori öğe verileri olan verileri tutan listeyi kayıtlara döndürüyoruz.
-    return orderListOfCurrentUser;
+
+    return ordersListOfCurrentUser;
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(16, 24, 8, 8),
-            child: Text(
-              "My Order List: ",
-              style: TextStyle(
-                  color: Colors.purpleAccent,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold),
+          //Order image       //history image
+          //myOrder title     //history title
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 24, 8, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //order icon image
+                // my orders
+                Column(
+                  children: [
+                    Image.asset(
+                      "assets/images/orders_icon.png",
+                      width: 140,
+                    ),
+                    const Text(
+                      "My Orders",
+                      style: TextStyle(
+                        color: Colors.purpleAccent,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+
+                //history icon image
+                // history
+                GestureDetector(
+                  onTap: () {
+                    //send user to orders history screen
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          "assets/images/history_icon.png",
+                          width: 45,
+                        ),
+                        const Text(
+                          "History",
+                          style: TextStyle(
+                            color: Colors.purpleAccent,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
 
-          const SizedBox(
-            height: 24,
+          //some info
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            child: Text(
+              "Here are your successfully placed orders.",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
           ),
-          Expanded(child: myOrderList(context))
-          //displaying order list
+
+          //displaying the user orderList
+          Expanded(
+            child: displayOrdersList(context),
+          ),
         ],
       ),
     );
   }
 
-  myOrderList(context) {
+  Widget displayOrdersList(context) {
     return FutureBuilder(
-      future: getCurrentOrderList(),
-      builder: (context, AsyncSnapshot<List<Order>> dataSnapShot) {
-        if (dataSnapShot.connectionState == ConnectionState.waiting) {
+      future: getCurrentUserOrdersList(),
+      builder: (context, AsyncSnapshot<List<Order>> dataSnapshot) {
+        if (dataSnapshot.connectionState == ConnectionState.waiting) {
           return const Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Center(child: Text("Connection waiting..")),
+              Center(
+                child: Text(
+                  "Connection Waiting...",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
               Center(
                 child: CircularProgressIndicator(),
               ),
             ],
           );
         }
-        if (dataSnapShot.data == null) {
-          return const Center(
-            child: Text(
-              "No order item found",
-              style: TextStyle(
-                color: Colors.grey,
+        if (dataSnapshot.data == null) {
+          return const Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  "No orders found yet...",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
               ),
-            ),
+              Center(
+                child: CircularProgressIndicator(),
+              ),
+            ],
           );
         }
-        if (dataSnapShot.data!.isNotEmpty) {
-          List<Order> orderList = dataSnapShot.data!;
+        if (dataSnapshot.data!.isNotEmpty) {
+          List<Order> orderList = dataSnapshot.data!;
 
           return ListView.separated(
-              itemBuilder: (context, index) {
-                Order eachOrderData = orderList[index];
+            padding: const EdgeInsets.all(16),
+            separatorBuilder: (context, index) {
+              return const Divider(
+                height: 1,
+                thickness: 1,
+              );
+            },
+            itemCount: orderList.length,
+            itemBuilder: (context, index) {
+              Order eachOrderData = orderList[index];
 
-                return Card(
-                  color: Colors.white24,
-                  child: Padding(
-                    padding: const EdgeInsets.all(18),
-                    child: ListTile(
-                      onTap: () {},
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Order ID # ${eachOrderData.order_id}",
-                            style: const TextStyle(
-                                fontSize: 16,
-                                color: Colors.grey,
-                                fontWeight: FontWeight.bold),
+              return Card(
+                color: Colors.white24,
+                child: Padding(
+                  padding: const EdgeInsets.all(18),
+                  child: ListTile(
+                    onTap: () {
+                      Get.to(OrderDetailsScreen(
+                        clickedOrderInfo: eachOrderData,
+                      ));
+                    },
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Order ID # ${eachOrderData.order_id}",
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Text(
-                            "Amount: \$  ${eachOrderData.totalAmount.toString()}",
-                            style: const TextStyle(
-                                fontSize: 16,
-                                color: Colors.purpleAccent,
-                                fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          //date
-                          //time
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                DateFormat(
-                                  "dd MMMM, yyyy",
-                                ).format(eachOrderData.dateTime!),
-                                style: const TextStyle(color: Colors.grey),
-                              ),
-                              const SizedBox(
-                                height: 4,
-                              ),
-                              //time
-                              Text(
-                                DateFormat(
-                                  "hh:mm a",
-                                ).format(eachOrderData.dateTime!),
-                                style: const TextStyle(color: Colors.grey),
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 4,
-                          ),
-
-                          const Icon(
-                            Icons.navigate_next,
+                        ),
+                        Text(
+                          "Amount: \$ ${eachOrderData.totalAmount}",
+                          style: const TextStyle(
+                            fontSize: 16,
                             color: Colors.purpleAccent,
-                          )
-                        ],
-                      ),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        //date
+                        //time
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            //date
+                            Text(
+                              DateFormat("dd MMMM, yyyy")
+                                  .format(eachOrderData.dateTime!),
+                              style: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+
+                            const SizedBox(height: 4),
+
+                            //time
+                            Text(
+                              DateFormat("hh:mm a")
+                                  .format(eachOrderData.dateTime!),
+                              style: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        const SizedBox(width: 6),
+
+                        const Icon(
+                          Icons.navigate_next,
+                          color: Colors.purpleAccent,
+                        ),
+                      ],
                     ),
                   ),
-                );
-              },
-              separatorBuilder: (context, index) {
-                return const Divider(
-                  height: 1,
-                  thickness: 1,
-                );
-              },
-              itemCount: orderList.length);
+                ),
+              );
+            },
+          );
         } else {
-          return const Center(
-            child: Text("Empty, No Data."),
+          return const Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  "Nothing to show...",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              Center(
+                child: CircularProgressIndicator(),
+              ),
+            ],
           );
         }
       },
