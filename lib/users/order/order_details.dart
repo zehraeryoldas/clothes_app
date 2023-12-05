8f455d484acfa7b99a6408fa321ebb98b102691e
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-12-05 16:58:47 +0300

    small changed

--- lib/users/order/order_details.dart
+++ lib/users/order/order_details.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:clothes_app/api_connection/api_connection.dart';
 import 'package:clothes_app/users/model/order.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
 
 class OrderDetailsScreen extends StatefulWidget {
   final Order? clickedOrderInfo;
@@ -18,6 +20,87 @@ class OrderDetailsScreen extends StatefulWidget {
 }
 
 class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
+  final RxString _status = "new".obs;
+  String get status => _status.value;
+
+  updateParcelStatusForUI(String parcelReceived) {
+    _status.value = parcelReceived;
+  }
+
+  showDialogForParcelConfirmation() async {
+    if (widget.clickedOrderInfo!.status == "new") {
+      var response = await Get.dialog(
+        AlertDialog(
+          backgroundColor: Colors.black,
+          title: const Text(
+            "Confirmation",
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+          content: const Text(
+            "Have you received your parcel?",
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Get.back();
+              },
+              child: const Text(
+                "No",
+                style: TextStyle(color: Colors.redAccent),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Get.back(result: "yesConfirmed");
+              },
+              child: const Text(
+                "Yes",
+                style: TextStyle(color: Colors.green),
+              ),
+            ),
+          ],
+        ),
+      );
+
+      if (response == "yesConfirmed") {
+        updateStatusValueInDatabase();
+      }
+    }
+  }
+
+  updateStatusValueInDatabase() async {
+    try {
+      var response = await http.post(Uri.parse(API.updateStatus), body: {
+        "order_id": widget.clickedOrderInfo!.order_id.toString(),
+      });
+
+      if (response.statusCode == 200) {
+        var responseBodyOfUpdateStatus = jsonDecode(response.body);
+        print("hata2");
+
+        if (responseBodyOfUpdateStatus["success"] == true) {
+          updateParcelStatusForUI("arrived");
+        }
+      }
+    } catch (e) {
+      print("hata");
+      print(e.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    updateParcelStatusForUI(widget.clickedOrderInfo!.status.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +112,51 @@ class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
               .format(widget.clickedOrderInfo!.dateTime!),
           style: const TextStyle(fontSize: 14),
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 8, 16, 8),
+            child: Material(
+              color: Colors.white30,
+              borderRadius: BorderRadius.circular(10),
+              child: InkWell(
+                onTap: () {
+                  if (status == "new") {
+                    showDialogForParcelConfirmation();
+                  }
+                },
+                borderRadius: BorderRadius.circular(30),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                  child: Row(
+                    children: [
+                      const Text(
+                        "Received",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Obx(() => status == "new"
+                          ? const Icon(
+                              Icons.help_outline,
+                              color: Colors.redAccent,
+                            )
+                          : const Icon(
+                              Icons.check_circle_outline,
+                              color: Colors.greenAccent,
+                            )),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -117,7 +245,7 @@ class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
               FadeInImage(
                 width: MediaQuery.of(context).size.width * 0.8,
                 fit: BoxFit.fitWidth,
-                placeholder: const AssetImage("assets/images/place_holder.png"),
+                placeholder: const AssetImage("images/place_holder.png"),
                 image: NetworkImage(
                   API.hostImages + widget.clickedOrderInfo!.image!,
                 ),
@@ -196,8 +324,7 @@ class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
                   height: 150,
                   width: 130,
                   fit: BoxFit.cover,
-                  placeholder:
-                      const AssetImage("assets/images/place_holder.png"),
+                  placeholder: const AssetImage("images/place_holder.png"),
                   image: NetworkImage(
                     itemInfo["image"],
                   ),
